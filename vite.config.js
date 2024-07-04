e523916037203ffcc49d69a0ebd92e64a0812299
esyaahmad <esya.ahmad@gmail.com>
2024-07-04 13:03:39 +0700

    test vercel -dev semua

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import { NAMA_PROGRAM } from "../config/config";
 
-const basename = '/ePemetaanBarang';
+const basename = '/ePemetaanBarang-dev';
 
 
 // https://vitejs.dev/config/
